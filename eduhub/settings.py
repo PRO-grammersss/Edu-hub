620f8ce66fbdb6ac26b3ca8cc4a72240b8c74a20
AdvaitaMehrotra <63447136+AdvaitaMehrotra@users.noreply.github.com>
2022-01-22 11:52:50 +0530

    Update settings.py

--- eduhub/settings.py
+++ eduhub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo.apps.todoconfig, notepad, main, login, forum'
 ]
 
 MIDDLEWARE = [
