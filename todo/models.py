71c6feb008c54509b08ad15fc1b4638ef32b8521
AdvaitaMehrotra <63447136+AdvaitaMehrotra@users.noreply.github.com>
2022-01-22 12:47:52 +0530

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=256)
+    description = models.TextField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['complete']
