952860fae059d183801c48932a9b72e542eff28b
AdvaitaMehrotra <63447136+AdvaitaMehrotra@users.noreply.github.com>
2022-01-22 19:56:31 +0530

    Update urls.py

--- eduhub/urls.py
+++ eduhub/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from login import views as loginviews
 from forum import views as forumviews
 
 
