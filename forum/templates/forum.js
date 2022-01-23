5e136765e203b1ea06707a1f1766617787cbdd43
Atira2009 <atiraharimohan@gmail.com>
2022-01-23 09:22:58 +0530

    Update forum.js

--- forum/templates/forum.js
+++ forum/templates/forum.js
@@ -10,3 +10,22 @@ function openFE() {
     input.click();
     
 }
+function post(){
+    var NewDiv = document.createElement('div');
+    NewDiv.id = 'output';
+    document.getElementsByTagName('body')[0].appendChild(NewDiv);
+    //var nid = document.createElement('div');
+    var title = document.getElementById('topic').value;
+    var msg = document.getElementById('myTextArea').value;
+    NewDiv.innerHTML = title + "<hr>" + msg;
+    var h1 = NewDiv.createElement('H1');
+    var h2 = NewDiv.createElement('H2');
+    h1 = title;
+    h2 = msg;
+    //var heading_with_tag = "";
+    //heading_with_tag = "<h2 id='jshtml'>"+ title + "</h2>"
+    //document.getElementById("finalheader").innerHTML += heading_with_tag;
+    //var msg = document.getElementById('myTextArea').value;
+    //message_with_tag = "<h3>"+ msg +"</h3>"
+    //document.getElementById("thePoSt").innerHTML += message_with_tag;
+}
