ff658d6c1591885e95f3bb0bf54e3f4815315682
AdvaitaMehrotra <63447136+AdvaitaMehrotra@users.noreply.github.com>
2022-01-23 08:03:16 +0530

    Update views.py

--- login/views.py
+++ login/views.py
@@ -6,29 +6,29 @@ from django.urls import reverse
 from django.views.generic.list import ListView
 from .models import *
 
-# Create your views here.
+
 def index(request):  #Function for the index view
-    if not request.user.is_authenticated or request.user == 'AnonymousUser': #User is not authenticated or not signed in 
-        return render(request, "ccr/login.html", { #Redirects the user to the login page if they try to access the home page without signing in 
+    if not request.user.is_authenticated or request.user == 'AnonymousUser':
+        return render(request, "login/login.html", { 
             'message': 'Sign in first'
         }) 
     else: #If user is authenticated and not anonymous 
-        return render(request, "ccr/index.html") #Sends the user to the home page
+        return render(request, "login/index.html") #Sends the user to the home page
 
 def login_view(request): 
     if request.method == "POST": #Uses the post method
-        username = request.POST["username"] #The variable for the username is created
-        password = request.POST["password"] #The variable for the password is created
-        user = authenticate(request, username=username, password=password) #The server authenticates the username and password
-        if user is not None: #when credentials are given
-            login(request, user) #Logs the user in 
-            return HttpResponseRedirect(reverse("index")) #Sends the user to the home page 
+        username = request.POST["username"] 
+        password = request.POST["password"] 
+        user = authenticate(request, username=username, password=password) 
+        if user is not None: 
+            login(request, user) 
+            return HttpResponseRedirect(reverse("index"))
         else: #If the credentials are invalid or wrong
-            return render(request, "ccr/login.html", { #Sends the user to the login page agian
-                "message": "Invalid credentials." #Says that the credentials are invalid
+            return render(request, "login/login.html", {
+                "message": "Invalid credentials." 
                 })
     #Post ends here
-    return render(request, "ccr/login.html") 
+    return render(request, "login/login.html") 
 
 def logout_view(request):
     logout(request)
